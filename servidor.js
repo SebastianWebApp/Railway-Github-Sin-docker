a6d4e65fe0f06182050f780fe1d649fed999411f
Mateo Espinosa <mateoespinosam@hotmail.com>
2025-05-11 18:13:16 -0500

    kk

--- servidor.js
+++ servidor.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.send("hola")
 });
 
+setInterval(() => {
+  console.log('Server is running...');
+}, 10000);  // Log cada 10 segundos
+
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
